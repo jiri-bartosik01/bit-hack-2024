00fd1481858a8c9187da917cf28da627fa2326c4
momocak997 <momocak997@scarden.com>
2024-10-10 17:02:37 +0200

    feat: Update preprocess.py

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -12,7 +12,9 @@ df = df.drop(columns=['verejnost', 'abonenti'])
 
 df['total'] = pd.to_numeric(df['total'], errors='coerce').fillna(0).astype(int)
 
-
+# Přiřazení čísel originálním názvům v prvním sloupci pomocí "enumerate"
+unique_places = {name: i for i, name in enumerate(df['place'].unique())}
+df['place'] = df['place'].map(unique_places)
 
 # Uložení upraveného souboru
-df.to_csv("data_zpracovane.csv", index=False, sep=';')
+df.to_csv("data_zpracovane_nove_nazvy.csv", index=False, sep=';')
