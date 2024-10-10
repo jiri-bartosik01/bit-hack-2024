cb8e94b80cfe3f6134b6b802b60f5f398a2004a2
Robert Blažek <35804312+robertblazek@users.noreply.github.com>
2024-10-10 13:36:57 +0200

    fix: Pool list with correct style

--- sapka/src/components/PoolListView.vue
+++ sapka/src/components/PoolListView.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
-
-import {pools} from "../helpers/pools.ts";
-
+import { pools } from "../helpers/pools.ts";
 </script>
 
 <template>
-  <div class="container">
-    <div class="card" style="width: 18rem;" v-for="pool in pools">
-      <img class="card-img-top" :src="pool.imageUrl" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">{{pool.name}}</h5>
-        <p class="card-text">{{pool.description}}</p>
-        <a href="#" class="btn btn-primary">Detail</a>
+
+  <div class="row gy-5">
+    <div class="col" v-for="pool in pools" :key="pool.id">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top custom-img" :src="pool.imageUrl" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">{{ pool.name }}</h5>
+          <p class="card-text">{{ pool.description }}</p>
+          <a href="#" class="btn btn-primary" @click="console.log(pool.id)">Detail</a>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.custom-img {
+  height: 100px;
+  object-fit: cover;
+  width: 100%;
+}
 
 </style>
