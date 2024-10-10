587824049cb552a70ee9ec63874ac33821f11463
Jiri Bartosik <jiri.bartosik.01@gmail.com>
2024-10-10 18:15:06 +0000

    feat: data

--- model/data.py
+++ model/data.py
@@ -8,12 +8,6 @@ from sklearn.model_selection import train_test_split
 file_path = '../preprocess/data_zpracovane.csv'  # Update if necessary to match the actual format
 data = pd.read_csv(file_path, sep=';')
 
-scaler = StandardScaler()
-data[['temperature']] = scaler.fit_transform(
-    data[['temperature']]
-)
-
-
 def create_time_windows(df, window_size=7):
     X, y = [], []
     for i in range(len(df) - window_size):
@@ -21,8 +15,9 @@ def create_time_windows(df, window_size=7):
         y.append(df.iloc[i + window_size]['total'])  # Target is the capacity on the next day
     return np.array(X), np.array(y)
 
+
 window_size = 168  # Number of days in each window
 X, y = create_time_windows(data, window_size)
 
 # Split into 70% training and 30% test
-train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, shuffle=False)
+train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, shuffle=True)
