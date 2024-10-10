047bb316fbce57ba80ad8d3e3847dee975f72738
Jiri Bartosik <jiri.bartosik.01@gmail.com>
2024-10-10 18:01:36 +0000

    feat: maybe final CNN model

--- model/model.py
+++ model/model.py
@@ -13,18 +13,18 @@ model = models.Sequential([
     layers.Conv1D(64, kernel_size=3, padding='SAME', activation='relu'),
     layers.BatchNormalization(),
     layers.MaxPooling1D(pool_size=2),
-    layers.Conv1D(128, kernel_size=3, padding='SAME', activation='relu'),
+    layers.Conv1D(248, kernel_size=3, padding='SAME', activation='relu'),
     layers.BatchNormalization(),
     layers.MaxPooling1D(pool_size=2),
     layers.Flatten(),
     layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-    layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+    layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
     layers.Dense(1)  # Output layer for regression
 ])
 
 # Compile the model
-#model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='mse', metrics=['mae'])
-model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='mse', metrics=['mae'])
+#model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
 # Summary of the model
 model.summary()
