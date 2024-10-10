9de89c28ecdf2d18d7824bae09ae1f2c4e65af91
Martin Gaens <djsushi123@gmail.com>
2024-10-10 13:26:04 +0200

    add: graph

--- sapka/src/App.vue
+++ sapka/src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
+import PredictionGraph from "./components/PredictionGraph.vue";
 </script>
 
 <template>
   <div class="card" style="width: 18rem;">
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">An item</li>
-      <li class="list-group-item">A second item</li>
-      <li class="list-group-item">A third item</li>
-    </ul>
+    <div class="container">
+      <PredictionGraph></PredictionGraph>
+    </div>
   </div>
 </template>
 
---
+++ sapka/src/components/PredictionGraph.vue
@@ -0,0 +1,44 @@
+<script setup lang="js">
+import {Chart, registerables} from "chart.js";
+import {onMounted, ref, useTemplateRef} from "vue";
+
+const canvas = useTemplateRef('canvas')
+
+onMounted(() => {
+  Chart.register(...registerables);
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const dataset = {
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }
+  const data = {
+    labels: labels,
+    datasets: [dataset]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+  new Chart(canvas.value, config);
+});
+
+</script>
+
+<template>
+  <div class="graph">
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+
+<style scoped>
+.graph {
+  width: 100%;
+  height: 100%;
+}
+</style>
